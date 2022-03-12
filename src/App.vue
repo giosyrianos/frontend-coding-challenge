256865213ca7fd6f1088e052af9e59fb85114184
gio <giosyrianos@gmail.com>
2022-03-12 16:00:51 +0200

    feat: adding navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
-<script setup></script>
 <template>
-	<router-view />
+	<NavbarMain />
+	<div class="container m-3">
+		<router-view />
+	</div>
 </template>
+<script>
+import NavbarMain from './components/Navbar-main.vue'
+export default {
+	name: 'App',
+	components: { NavbarMain },
+}
+</script>
 <style></style>
