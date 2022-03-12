166873ec9f0206695d340867c45629f5b3d91f3a
gio <giosyrianos@gmail.com>
2022-03-12 18:48:38 +0200

    feat: load projects from api

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore } from 'vuex'
+import ProjectsDataService from '../services/ProjectsDataService'
+// import { router } from '../router'
 
 // import splitProjectsModule from './modules/splitProjects'
 
@@ -9,9 +11,22 @@ const store = createStore({
 		}
 	},
 
-	actions: {},
-	mutations: {},
-	getters: {}
+	actions: {
+		fetchProjectList ({ commit }) {
+			ProjectsDataService.getAllProjects().then((listData) => {
+				commit('SET_PROJECTS_LIST', listData.data)
+			})
+		}
+	},
+	mutations: {
+		SET_PROJECTS_LIST (state, payload) {
+			console.log(payload)
+			state.projectsList = payload
+		}
+	},
+	getters: {
+		loadProjects: (state) => state.projectsList
+	}
 
 	// modules: {
 	// 	splitProjects: splitProjectsModule
--- src/views/Projects-list.vue
+++ src/views/Projects-list.vue
@@ -1,8 +1,32 @@
 <template>
 	<div>
-		<h3>This is Projects</h3>
-		<button class="btn btn-primary">
-			Click mE
-		</button>
+		<ul class="list-group">
+			<li
+				v-for="project in loadedProjects"
+				:key="project.id"
+				class="list-group-item"
+			>
+				{{ project.name }}
+			</li>
+		</ul>
 	</div>
 </template>
+
+<script>
+export default {
+	name: 'ProjectsList',
+	computed: {
+		loadedProjects () {
+			return this.$store.getters.loadProjects
+		}
+	},
+	mounted () {
+		this.getProjects()
+	},
+	methods: {
+		getProjects () {
+			this.$store.dispatch('fetchProjectList')
+		}
+	}
+}
+</script>
